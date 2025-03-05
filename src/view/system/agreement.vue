cda6ba37c17fe7180fe6ec42f1200d92aab7d720
is-wsq <103235337+is-wsq@users.noreply.github.com>
2025-03-05 10:31:27 +0800

    update

--- src/view/system/agreement.vue
+++ src/view/system/agreement.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="agreement">
-    <h2>用户协议 & 隐私政策</h2>
-
-    <!-- 选择查看哪个文档 -->
-    <button @click="previewWord('userAgreement')">查看用户协议</button>
-    <button @click="previewWord('privacyPolicy')">查看隐私政策</button>
-
-    <!-- 预览 Word 文件 -->
-    <div v-if="selectedFile">
-      <h3>{{ selectedFile === 'userAgreement' ? '用户协议' : '隐私政策' }}</h3>
+    <div>
+      <el-radio-group v-model="type">
+        <el-radio-button label="声音克隆协议"></el-radio-button>
+        <el-radio-button label="形象克隆协议"></el-radio-button>
+      </el-radio-group>
+    </div>
+    <div class="iframe-container">
       <iframe
-          :src="localFiles"
-          width="100%"
-          height="600px"
-          frameborder="0"
+        :src="docUrl"
+        width="100%"
+        height="100%"
+        frameborder="0"
       ></iframe>
     </div>
   </div>
@@ -23,24 +21,30 @@
 export default {
   data() {
     return {
-      selectedFile: '', // 记录当前选中的文件
-      wordFiles: {
-        wordFileUrl1: window.location.origin + '/timbre-clone.docx',
-        wordFileUrl2: window.location.origin + '/figure-clone.docx',
-      },
-      localFiles: 'file:///D:/Documents/timbre-clone.docx'
+      type: "声音克隆协议",
+      docFile: "", // 替换为你的 .doc 文件地址
     };
   },
   computed: {
-    wordPreviewUrl() {
-      if (!this.selectedFile) return '';
-      return `https://view.officeapps.live.com/op/view.aspx?src=${encodeURIComponent(this.wordFiles[this.selectedFile])}`;
+    docUrl() {
+      this.type === "声音克隆协议"
+       ? (this.docFile = "https://live.tellai.tech/files/timbre-clone.doc")
+       : (this.docFile = "https://live.tellai.tech/files/figure-clone.doc");
+      return `https://docs.google.com/gview?url=${encodeURIComponent(this.docFile)}&embedded=true`;
     },
   },
   methods: {
-    previewWord(fileKey) {
-      this.selectedFile = fileKey; // 设置选中的文档
-    },
+
   },
 };
 </script>
+
+<style scoped>
+.agreement {
+  height: 100%;
+}
+.iframe-container {
+  height: calc(100% - 45px);
+  margin-top: 5px;
+}
+</style>
