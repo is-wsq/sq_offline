6cd87902cf073261b3310ae7cf5804deac9a8b27
is-wsq <103235337+is-wsq@users.noreply.github.com>
2025-05-26 09:19:56 +0800

    引入字体库

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import './assets/iconfont/iconfont.css'
+import './assets/fonts/font.css';
 
 import store from './store';
 import {Notification} from 'element-ui';
