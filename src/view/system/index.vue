4f022671f13e0690a60fbbbe8baef3d19fe066a2
is-wsq <103235337+is-wsq@users.noreply.github.com>
2025-06-03 16:48:49 +0800

    联系我们

--- src/view/system/index.vue
+++ src/view/system/index.vue
@@ -4,7 +4,7 @@
       <div class="title" @click="goto('/agreement')">用户协议</div>
       <div class="title">隐私政策</div>
       <div class="title">产品说明</div>
-      <div class="title">联系我们</div>
+      <div class="title" @click="contactUs">联系我们</div>
       <el-form :inline="true" label-width="100px">
         <el-form-item label="保存路径">
           <el-input v-model="downloadPath" placeholder="请选择视频下载保存路径" style="width: 400px;" readonly/>
@@ -12,6 +12,14 @@
         </el-form-item>
       </el-form>
     </div>
+    <el-dialog class="contact-us-dialog" title="联系我们" :visible.sync="dialogVisible" width="500px">
+      <div class="contact-us">
+        <div style="font-size: 15px; font-weight: bold;color: #000000;margin: 10px 0;">广东省奇点未来软件研发</div>
+        <el-image style="width: 200px; height: 200px;" :src="require('/public/images/qrcode.png')" fit="cover">
+        </el-image>
+        <div style="margin-top: 20px;font-size: 14px;color: #000000;">使用手机微信扫一扫，添加客服微信</div>
+      </div>
+    </el-dialog>
   </div>
 </template>
 
@@ -20,6 +28,7 @@ export default {
   name: 'system',
   data() {
     return {
+      dialogVisible: false,
       downloadPath: ''
     }
   },
@@ -27,6 +36,9 @@ export default {
     this.downloadPath = localStorage.getItem('downloadPath') || 'D:\\offline'
   },
   methods: {
+    contactUs() {
+      this.dialogVisible = true
+    },
     chooseFolder() {
       window.electronAPI.selectFolder().then((path) => {
         if (path) {
@@ -72,5 +84,15 @@ export default {
   text-align: center;
   color: #101010;
   cursor: pointer;
+}
+
+.contact-us-dialog >>> .el-dialog__body {
+  padding-top: 0 !important;
+}
+
+.contact-us {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
